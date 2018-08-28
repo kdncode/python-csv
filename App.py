55ca2b59ba5845f060c4c75d023c8ea1462ffce1
kdncode <kdncode@gmail.com>
2018-08-27 23:57:16 -0500

    updated

--- App.py
+++ App.py
@@ -6,7 +6,6 @@ with open('App.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     for line in csv_reader:
+        numbers = line.split('/')
         arr.append(line)
     print(arr)
-        
-
